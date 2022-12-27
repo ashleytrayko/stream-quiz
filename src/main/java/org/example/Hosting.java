f37c854342e6c01a99f5ce7ec54e41dcb6738fb2
고형진 <aion7304@gmail.com>
2022-12-27 18:08:26 +0900

    22-12-27 18:07 Stream 연습
    List -> Map 연습

---
+++ src/main/java/org/example/Hosting.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Hosting {
+    private int Id;
+    private String name;
+    private long websites;
+
+}
